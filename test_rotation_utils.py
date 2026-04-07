7a0d80a4a609ee22b1e67aef9e7c3249d21f0f64
Rajani <rajani.kami004@gmail.com>
2026-04-07 00:08:35 -0400

    def test positive

---
+++ test_rotation_utils.py
@@ -0,0 +1,24 @@
+"""
+Program: test_rotation_utils.py
+Author: Rajani Baraili
+Purpose: verifies the adjust_rotation() function from
+         rotation_utils.py,handles positive rotations, negative rotations,
+         non-numeric inputs, and various edge cases correctly.
+Starter Code: rotation_utils.py provided by Gabriel Walters (October 27, 2025).
+Date: April 6, 2026
+"""
+import pytest 
+from rotation_utils import adjust_rotation
+
+
+def test_positive_100():
+    assert adjust_rotation(100) == 100
+
+def test_positive_460():
+    assert adjust_rotation(460) == 100
+
+def test_positive_820():
+    assert adjust_rotation(820) == 100 
+
+
+
